abed877419455e07718356e6bdca6ed564619762
LixianSu <lixiansu.ac@gmail.com>
2023-05-16 10:59:50 +0800

    Update Sectors to column.py

--- Preprocessing/Sectors to column.py
+++ Preprocessing/Sectors to column.py
@@ -1,27 +1,28 @@
 import pandas as pd
-import chardet
+import os
 
 
-file = r"C:\Software\Russian-Ukrainian War\Data\Recent\USA\USA_EXPORT.csv"
-
-df = pd.read_csv(file, low_memory=False)
+path = r"C:\Software\Russian-Ukrainian War\Data\Recent\USA"
+file = r"USA_EXPORT.csv"
+df = pd.read_csv(os.path.join(path,file), low_memory=False)
 newd = {'source':[], 'date':[], 'agriculture':[], 'apparel':[], 'chemicals':[], 'machinery':[], 'materials':[], 'metals':[], 'miscellaneous':[], 'transport':[], 'minerals':[]}
 
-initial_date = df.loc[1, "date"]
-initial_source = df.loc[1, "source"]
-initial_sector = df.loc[1, "sector"]
-initial_value = df.loc[1, "value"]
+initial_date = df.loc[0, "date"]
+initial_source = df.loc[0, "source"]
+initial_sector = df.loc[0, "sector"]
+initial_value = df.loc[0, "value"]
 
 inspector = ['agriculture', 'apparel', 'chemicals', 'machinery', 'materials', 'metals', 'miscellaneous',
-                     'transport,minerals']
+                     'transport', 'minerals']
 
 newd['source'].append(initial_source)
 newd['date'].append(initial_date)
 newd[initial_sector].append(initial_value)
+print(initial_sector)
 inspector.remove(initial_sector)
 
 
-for i in range(2, len(df)):
+for i in range(1, len(df)):
     idate = df.loc[i, "date"]
     isource = df.loc[i, "source"]
     isector = df.loc[i, "sector"]
@@ -29,13 +30,13 @@ for i in range(2, len(df)):
 
     if idate != initial_date or isource != initial_source:
         if inspector:
-            print("finish one loop\n")
-        else:
             for i in inspector:
                 newd[i].append(0)
-            print("finish one loop\n")
+            print("finish one loop.\n")
+        else:
+            print("finish one loop.\n")
         inspector = ['agriculture', 'apparel', 'chemicals', 'machinery', 'materials', 'metals', 'miscellaneous',
-                     'transport,minerals']
+                     'transport', 'minerals']
         newd['source'].append(isource)
         newd['date'].append(idate)
         newd[isector].append(ivalue)
@@ -46,5 +47,12 @@ for i in range(2, len(df)):
         inspector.remove(isector)
         newd[isector].append(ivalue)
 
+if inspector:
+    for i in inspector:
+        newd[i].append(0)
+        print("finish one loop.\n")
+    else:
+        print("finish one loop.\n")
+
 newdf = pd.DataFrame(newd)
-newdf.to_csv('DataFrame_ab.csv', index=False, sep=',')
+newdf.to_csv(os.path.join(path, 'DataFrame_ab.csv'), index=False, sep=',')
